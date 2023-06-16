e4da0cd903a2d22166a2266000ace357f9a6ac94
Mohammed Alabdulhadi <mohammad588577@gmail.com>
2023-06-16 23:15:12 +0300

    add5repoTestAnd5ServiceTest

---
+++ src/test/java/com/example/tanfeeth/OperationCompanyRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.example.tanfeeth;
+
+
+import com.example.tanfeeth.Model.MyUser;
+import com.example.tanfeeth.Model.OperationCompany;
+import com.example.tanfeeth.Repository.MyUserRepositroy;
+import com.example.tanfeeth.Repository.OperationCompanyRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class OperationCompanyRepositoryTest {
+
+    @Autowired
+    MyUserRepositroy myUserRepositroy;
+    @Autowired
+    OperationCompanyRepository operationCompanyRepository;
+
+    MyUser myUser;
+    OperationCompany operationCompany;
+    List<String> strings = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        strings.add("test1");
+        strings.add("test2");
+        myUser = new MyUser(null,"m@gmail.com","329487","0530503434","OPERATION",null,null);
+        operationCompany=new OperationCompany(null,"test",0.0,"138049283","3244234","test",strings,"it",myUser,null,null,null);
+    }
+
+    @Test
+    void findOperationCompanyByIdTest() {
+        myUserRepositroy.save(myUser);
+        operationCompanyRepository.save(operationCompany);
+        OperationCompany operationCompany1 = operationCompanyRepository.findOperationCompanyById(myUser.getId());
+        Assertions.assertThat(operationCompany1).isEqualTo(operationCompany);
+    }
+
+    @Test
+    void findOperationCompanyByNameTest() {
+        myUserRepositroy.save(myUser);
+        operationCompanyRepository.save(operationCompany);
+        OperationCompany operationCompany1 = operationCompanyRepository.findOperationCompanyByName(operationCompany.getName());
+        Assertions.assertThat(operationCompany1).isEqualTo(operationCompany);
+    }
+}
---
+++ src/test/java/com/example/tanfeeth/StaffServiceTest.java
@@ -0,0 +1,50 @@
+package com.example.tanfeeth;
+
+
+import com.example.tanfeeth.Model.Staff;
+import com.example.tanfeeth.Repository.StaffRepository;
+import com.example.tanfeeth.Service.StaffService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+
+public class StaffServiceTest {
+
+    @InjectMocks
+    StaffService staffService;
+    @Mock
+    StaffRepository staffRepository;
+
+    Staff staff,staff1,staff2;
+    List<Staff> staffList = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        staff = new Staff(null,"mohammed1",23,"M","it","saudi",null,null,null,null);
+        staff1 = new Staff(null,"mohammed2",23,"M","it","saudi",null,null,null,null);
+        staff2 = new Staff(null,"mohammed3",23,"M","it","saudi",null,null,null,null);
+        staffList.add(staff);
+        staffList.add(staff1);
+        staffList.add(staff2);
+    }
+
+    @Test
+    void getAllStaffForAllCompanyTest() {
+        when(staffRepository.findAll()).thenReturn(staffList);
+        List<Staff> staffList1 =staffService.getAllStaffForAllCompany();
+        Assertions.assertEquals(staffList1,staffList);
+        verify(staffRepository,times(1)).findAll();
+    }
+
+}
